4a614821fd35f91eaefc31013fcb8a201e76fcb4
me <lars.scheme@gmail.com>
2015-07-06 17:45:20 +0200

    Fixed compilation error for linux.

--- draw/sound_linux.go
+++ draw/sound_linux.go
@@ -2,6 +2,6 @@ package draw
 
 import "errors"
 
-func (w *window) playSoundFile(path string) error {
+func playSoundFile(path string) error {
 	return errors.New("not yet implemented on this OS")
 }
