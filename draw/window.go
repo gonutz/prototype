d48799550e9b261dd8827732f73091f40530be74
me <lars.scheme@gmail.com>
2015-07-25 18:29:27 +0200

    Added color constructors.

--- draw/window.go
+++ draw/window.go
@@ -53,6 +53,14 @@ const (
 
 type Color struct{ R, G, B, A float32 }
 
+func RGB(r, g, b float32) Color {
+	return Color{r, g, b, 1}
+}
+
+func RGBA(r, g, b, a float32) Color {
+	return Color{r, g, b, a}
+}
+
 var Black = Color{0, 0, 0, 1}
 var White = Color{1, 1, 1, 1}
 var Gray = Color{0.5, 0.5, 0.5, 1}
