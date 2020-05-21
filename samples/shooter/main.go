2da73ce7ea656814cd0022f854ffceed7c7bca6a
lars <lars.scheme@gmail.com>
2020-05-21 09:25:40 +0200

    Refactor shooter sample, no GOPATH use anymore

--- samples/shooter/main.go
+++ samples/shooter/main.go
@@ -1,128 +1,136 @@
 package main
 
-import (
-	"github.com/gonutz/prototype/draw"
-	"math"
-	"os"
-	"path/filepath"
+import "github.com/gonutz/prototype/draw"
+
+const (
+	windowWidth        = 640
+	windowHeight       = 480
+	shipSize           = 20
+	shipSpeed          = 5
+	bulletDelay        = 10
+	patternChangeDelay = 20
+	enemySize          = 30
+	bulletSize         = 8
+	bulletSpeed        = 8
 )
 
 func main() {
-	ship := &spaceShip{310, 450}
-	const speed = 5
-	var bullets []*bullet
-	nextBullet := 0
-	const bulletDelay = 10
-	enemies := spreadEnemies()
-	movePatterns := []struct{ dx, dy int }{
-		{-1, 0}, {-1, 0},
-		{0, 1},
-		{1, 0}, {1, 0}, {1, 0}, {1, 0},
-		{0, 1},
-		{-1, 0}, {-1, 0},
-	}
-	pattern := 0
-	const patternChangeDelay = 20
-	nextPatternChange := patternChangeDelay
-	gameOver := false
-	gameOverText := ""
+	var (
+		ship              spaceShip
+		bullets           []bullet
+		nextBulletLock    int // Only shoot at bulletDelay intervals.
+		enemies           []enemy
+		enemyMovePattern  int
+		nextPatternChange int
+		gameOver          bool
+		gameOverText      string
 
-	reset := func() {
-		ship = &spaceShip{310, 450}
+		// Enemies move left, down, right, down, left, down, ...
+		movePatterns = []struct{ dx, dy int }{
+			{-1, 0}, {-1, 0},
+			{0, 1},
+			{1, 0}, {1, 0}, {1, 0}, {1, 0},
+			{0, 1},
+			{-1, 0}, {-1, 0},
+		}
+	)
+
+	newGame := func() {
+		ship = spaceShip{
+			x: (windowWidth - shipSize) / 2,
+			y: windowHeight - 10 - shipSize,
+		}
 		bullets = nil
-		nextBullet = 0
-		enemies = spreadEnemies()
-		pattern = 0
+		nextBulletLock = 0
+		enemies = createEnemies()
+		enemyMovePattern = 0
 		nextPatternChange = patternChangeDelay
 		gameOver = false
 		gameOverText = ""
 	}
+	newGame()
 
-	mainErr := draw.RunWindow("Space Shooter", 640, 480, func(window draw.Window) {
+	err := draw.RunWindow("Space Shooter", windowWidth, windowHeight, func(window draw.Window) {
 		if window.WasKeyPressed(draw.KeyEscape) {
 			window.Close()
 		}
 		if window.WasKeyPressed(draw.KeyN) {
-			reset()
+			newGame()
 		}
 
 		if !gameOver {
+
 			if window.IsKeyDown(draw.KeyLeft) {
-				ship.move(-speed)
+				ship.move(-shipSpeed)
 			}
 			if window.IsKeyDown(draw.KeyRight) {
-				ship.move(speed)
+				ship.move(shipSpeed)
 			}
+			nextBulletLock--
 			if window.IsKeyDown(draw.KeySpace) || window.WasKeyPressed(draw.KeySpace) {
-				if nextBullet <= 0 {
-					bullets = append(bullets,
-						&bullet{x: ship.x + 10, y: ship.y - 5})
-					nextBullet = bulletDelay
-					window.PlaySoundFile(resourcePath("shoot.wav"))
+				if nextBulletLock <= 0 {
+					bullets = append(
+						bullets,
+						bullet{x: ship.x + shipSize/2, y: ship.y + shipSize/2},
+					)
+					nextBulletLock = bulletDelay
+					window.PlaySoundFile("shoot.wav")
 				}
 			}
-			nextBullet--
-			nextPatternChange--
 
-			if nextPatternChange <= 0 {
-				nextPatternChange = patternChangeDelay
-				pattern = (pattern + 1) % len(movePatterns)
-			}
-			p := movePatterns[pattern]
-			for _, e := range enemies {
-				e.move(p.dx, p.dy)
-			}
-			for _, b := range bullets {
-				b.move(-8)
-				for _, e := range enemies {
-					b.collide(e, window)
+			for i := range bullets {
+				bullets[i].y -= bulletSpeed
+				for j := range enemies {
+					bullets[i].collide(&enemies[j], window)
 				}
 			}
 			bullets = removeDeadBullets(bullets)
-			enemies = removeDeadEnemies(enemies)
 
+			nextPatternChange--
+			if nextPatternChange <= 0 {
+				nextPatternChange = patternChangeDelay
+				enemyMovePattern = (enemyMovePattern + 1) % len(movePatterns)
+			}
+			p := movePatterns[enemyMovePattern]
+			for i := range enemies {
+				enemies[i].move(p.dx, p.dy)
+			}
+			enemies = removeDeadEnemies(enemies)
 			if len(enemies) == 0 {
 				gameOver = true
 				gameOverText = "You defeated the enemy!\n  Press n to restart."
 			}
 			for _, e := range enemies {
-				if e.y >= 450 {
+				if e.y >= ship.y {
 					gameOver = true
 					gameOverText = " You lost the battle...\n   Press n to restart"
 				}
 			}
 		}
 
-		ship.draw(window)
+		window.FillEllipse(ship.x, ship.y, shipSize, shipSize, draw.Red)
 		for _, b := range bullets {
-			b.draw(window)
+			window.FillEllipse(b.x-bulletSize/2, b.y-bulletSize/2, bulletSize, bulletSize, draw.Green)
 		}
 		for _, e := range enemies {
-			e.draw(window)
+			window.FillRect(e.x-enemySize/2, e.y-enemySize/2, enemySize, enemySize, e.color)
 		}
 		window.DrawScaledText(gameOverText, 100, 180, 2.0, draw.White)
 	})
 
-	if mainErr != nil {
-		panic(mainErr)
+	if err != nil {
+		panic(err)
 	}
 }
 
-func resourcePath(filename string) string {
-	projectPath := filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "gonutz", "prototype", "samples", "shooter")
-	return filepath.Join(projectPath, filename)
-}
-
-func spreadEnemies() []*enemy {
-	enemies := make([]*enemy, 0, 50)
-	const size = 30
+func createEnemies() []enemy {
+	var enemies []enemy
 	colors := []draw.Color{draw.DarkBlue, draw.Blue, draw.LightBlue}
 	for y := 0; y < 3; y++ {
-		for x := 70; x <= 640-50-size/2; x += 25 + size {
-			enemies = append(enemies, &enemy{
+		for x := 70; x <= 640-50-enemySize/2; x += 25 + enemySize {
+			enemies = append(enemies, enemy{
 				x:     x,
-				y:     30 + y*(size+10),
-				size:  size,
+				y:     30 + y*(enemySize+10),
 				life:  3 - y,
 				color: colors[y],
 			})
@@ -132,11 +140,7 @@ func spreadEnemies() []*enemy {
 }
 
 type spaceShip struct {
-	x, y int
-}
-
-func (s *spaceShip) draw(window draw.Window) {
-	window.FillEllipse(s.x, s.y, 20, 20, draw.Red)
+	x, y int // (x,y) is the top-left of the square ship.
 }
 
 func (s *spaceShip) move(dx int) {
@@ -150,60 +154,41 @@ func (s *spaceShip) move(dx int) {
 }
 
 type bullet struct {
-	x, y int
+	x, y int // (x,y) is the center of the circular bullet.
 	dead bool
 }
 
-const bulletSize = 8
-
-func (b *bullet) draw(window draw.Window) {
-	window.FillEllipse(b.x-bulletSize/2, b.y-bulletSize/2, bulletSize, bulletSize, draw.Green)
-}
-
-func (b *bullet) move(dy int) {
-	b.y += dy
-}
-
 func (b *bullet) collide(e *enemy, window draw.Window) {
-	dx := abs(b.x - e.x)
-	dy := abs(b.y - e.y)
-	dist := int(math.Sqrt(float64(dx*dx+dy*dy)) + 0.5)
-	if dist <= e.size/2+bulletSize/2 {
+	squareDist := square(b.x-e.x) + square(b.y-e.y)
+	if squareDist <= square(enemySize/2+bulletSize/2) {
 		e.life--
 		b.dead = true
 		if e.life == 0 {
-			window.PlaySoundFile(resourcePath("explosion.wav"))
+			window.PlaySoundFile("explosion.wav")
 		} else {
-			window.PlaySoundFile(resourcePath("hit.wav"))
+			window.PlaySoundFile("hit.wav")
 		}
 	}
 }
 
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
+func square(x int) int {
+	return x * x
 }
 
-func removeDeadBullets(bullets []*bullet) []*bullet {
-	inBounds := make([]*bullet, 0, len(bullets))
+func removeDeadBullets(bullets []bullet) []bullet {
+	alive := bullets[:0] // Reuse the array.
 	for _, b := range bullets {
 		if b.y > -100 && !b.dead {
-			inBounds = append(inBounds, b)
+			alive = append(alive, b)
 		}
 	}
-	return inBounds
+	return alive
 }
 
 type enemy struct {
-	x, y, size int
-	life       int
-	color      draw.Color
-}
-
-func (e *enemy) draw(window draw.Window) {
-	window.FillRect(e.x-e.size/2, e.y-e.size/2, e.size, e.size, e.color)
+	x, y  int
+	life  int
+	color draw.Color
 }
 
 func (e *enemy) move(dx, dy int) {
@@ -211,8 +196,8 @@ func (e *enemy) move(dx, dy int) {
 	e.y += dy
 }
 
-func removeDeadEnemies(enemies []*enemy) []*enemy {
-	alive := make([]*enemy, 0, len(enemies))
+func removeDeadEnemies(enemies []enemy) []enemy {
+	alive := enemies[:0] // Reuse the array.
 	for _, e := range enemies {
 		if e.life > 0 {
 			alive = append(alive, e)
